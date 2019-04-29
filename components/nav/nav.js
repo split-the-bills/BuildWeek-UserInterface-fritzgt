ff9e3a0776ff76409fb0cf1c91a4dac9ec425c60
fritzgt <tuanis@live.com>
2019-04-28 22:30:24 -0400

    create js class for dropdown

---
+++ components/nav/nav.js
@@ -0,0 +1,17 @@
+class Dropdown {
+    constructor(element) {
+        this.element = element;
+        this.button = this.element.querySelector(".dropdown-button");
+        this.content = this.element.querySelector(".dropdown-content");
+        this.button.addEventListener("click", () => {
+            this.toggleContent();
+        });
+    }
+
+    toggleContent() {
+        this.content.classList.toggle("dropdown-hidden");
+    }
+}
+
+let dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach(dropdown => new Dropdown(dropdown));
